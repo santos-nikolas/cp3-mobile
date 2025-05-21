a981890aee4a2637d94b0b868d7ece008e006202
santos-nikolas <rm551566@fiap.com.br>
2025-05-21 18:42:25 -0300

    extraindo dados da tag fiction da API

--- src/Screens/Fiction.tsx
+++ src/Screens/Fiction.tsx
@@ -1,7 +1,22 @@
-import { Text } from "react-native"
+import { useEffect, useState } from "react";
+import { dummyApi } from "@/api";
+import { Post, PostResponse } from "@/types";
 
-const Fiction =() => {
-    return <Text>Fiction</Text>
-}
+import List from "@/Components/Posts/List";
+
+const Fiction = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  const fetchPosts = async () => {
+    const { data } = await dummyApi.get<PostResponse>(`posts/tag/fiction`);
+    setPosts(data.posts);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return <List posts={posts} />;
+};
 
 export default Fiction
